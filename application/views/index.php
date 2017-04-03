d70f3713dd5112e7e39af42de6832e130f1fdb82
elegant1225 <1737289219@qq.com>
2017-04-03 19:44:28 +0800

    完善项目初始化配置，并写了首页header，以及自己手动写响应式

---
+++ application/views/index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>首页</title>
+    <meta name="viewport" content="width=device-width,initial-scale=1.0"/>
+    <base href="<?php echo site_url();?>">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+    <header class="banner">
+        <div class="container">
+            <div class="b-part b-part1">
+                <img src="img/1.jpg" alt=" "/>
+                <h2>Kristin Stewart</h2>
+                <h3>Web Designer</h3>
+            </div>
+            <div class="b-part b-part2">
+                <h3>Contact</h3>
+                <h4><a href="mailto:info@example.com">exam@gmail.com</a></h4>
+                <h4 class="agile">+18044261158</h4>
+                <h4>Richmond,USA</h4>
+            </div>
+            <div class="b-part b-part3">
+                <h3>Follow</h3>
+                <a href="#" class="icon"><i class="icon-3x" ></i></a>
+                <a href="#" class="icon"><i class="fa-"></i></a>
+                <a href="#" class="icon"><i class=""></i></a>
+                <a href="#" class="icon"><i class="icon-pencil"></i></a>
+            </div>
+            <div class="b-part b-part4">
+                <h3>Visit</h3>
+                <i class="fa fa-behance" aria-hidden="true"></i>
+                <h4>Follow my behance portfolio</h4>
+            </div>
+            <div class="clearfix"></div>
+        </div>
+        </div>
+    </header>
+    <div id="about">
+        <div class="container">
+            
+        </div>
+    </div>
+    <div id="skills">
+        <div class="container">
+
+        </div>
+    </div>
+    <div id="protfolia">
+        <div class="container"></div>
+    </div>
+    <div id="contect">
+        <div class="container">
+
+        </div>
+    </div>
+    <footer id="footer">
+        <div class="container"></div>
+    </footer>
+<script src="js/jquery-1.12.4.min.js"></script>
+</body>
+</html>
