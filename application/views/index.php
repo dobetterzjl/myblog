ddf6f73ba251041217a55cd652090b97a2344899
elegant1225 <1737289219@qq.com>
2017-04-03 20:38:07 +0800

    完成about页面及其响应式

--- application/views/index.php
+++ application/views/index.php
@@ -25,9 +25,9 @@
             </div>
             <div class="b-part b-part3">
                 <h3>Follow</h3>
-                <a href="#" class="icon"><i class="icon-3x" ></i></a>
-                <a href="#" class="icon"><i class="fa-"></i></a>
-                <a href="#" class="icon"><i class=""></i></a>
+                <a href="#" class="icon"><i class="icon-3x icon-github" ></i></a>
+                <a href="#" class="icon"><i class="icon-camera-retro"></i> </a>
+                <a href="#" class="icon"> <i class="icon-github-alt"></i> </a>
                 <a href="#" class="icon"><i class="icon-pencil"></i></a>
             </div>
             <div class="b-part b-part4">
@@ -41,7 +41,22 @@
     </header>
     <div id="about">
         <div class="container">
-            
+                <h3>ABOUT ME</h3>
+                <span class="line"></span>
+                <img src="img/1.jpg">
+                <p class="introduce">
+                    张建丽：黑龙江大学，计算机科学技术学院，物联网工程专业2014级学生，
+                    从大二开始接触前端并利用课余时间学习前端，能熟练使用html，CSS进行各种网页布局
+                    利用原生js或jQuery完成网页各种交互，了解jQuery原理，并自己编写过简单的jQuery库
+                    熟悉h5,css3，熟悉bootstrap，并能自己手写响应式，了解浏览器兼容性问题，服务器端语言
+                    了解nodejs,php以及它的CI框架，并用CI框架搭建过两个项目，了解mysql
+                </p>
+                <p class="evaluation">
+                    自我评价：能借助各种工具进行英文文档阅读，
+                    乐于接受新知识，对前端领域有很强的探索精神
+                    学习能力，责任感较强，能承受压力及时完成任务，做事耐心细致；
+                    本人性格较为沉稳，上进心强，有较强的责任心和良好的团队合作精神。
+                </p>
         </div>
     </div>
     <div id="skills">
